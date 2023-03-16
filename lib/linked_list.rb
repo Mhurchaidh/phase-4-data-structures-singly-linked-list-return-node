12338fa18e862ce217ad39150f78d266d5ac393c
mhurchaidh <bmurchie1997@gmail.com>
2023-03-16 13:35:07 -0700

    commit tree a7eda6dc7c9523e0306852a3aacc12cb8810debe

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,36 @@ class LinkedList
     @head = nil
   end
 
+  # Option 1
+  # def list_length
+
+  # end
+
   def nth_from_end(n)
-    # your code here
+    n1 = @head
+    n2 = @head
+
+    i = 0
+
+    if n < 1 || @head.nil?
+      return nil
+    end
+    
+    while i < n - 1
+      n1 = n1.next_node
+      i += 1
+      if n1.nil? 
+        return nil
+      end
+    end
+
+    while n1.next_node
+      n1 = n1.next_node
+      n2 = n2.next_node
+    end
+
+    return n2.value
+
   end
 
 end
